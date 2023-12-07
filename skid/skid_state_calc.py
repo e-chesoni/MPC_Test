5123b191e944f50eb81da9553795eceaf644160b
Elaine Chesoni <elaine.chesoni@gmail.com>
2023-12-06 23:02:21 -0500

    Able to run MPC with iLQR Trajectory
    MPC still drives in a circle regardless of iLQR solution

--- skid/skid_state_calc.py
+++ skid/skid_state_calc.py
@@ -3,7 +3,7 @@ from scipy.integrate import solve_ivp
 from skid.skid_steer_system import SkidSteerSystem
 
 
-class Skid_Steer_Simulator():
+class SkidSteerCalculateState():
   def __init__(self):
     pass
 
@@ -23,6 +23,6 @@ class Skid_Steer_Simulator():
   def F(xc, uc, dt):
     # Simulate the open loop skid steer for one step
     def f(_, x):
-      return Skid_Steer_Simulator.f(x, uc)
+      return SkidSteerCalculateState.f(x, uc)
     sol = solve_ivp(f, (0, dt), xc, first_step=dt)
     return sol.y[:, -1].ravel()
